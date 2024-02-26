02e4fff2fd3b888821aac1dd5c1ccc3e496cad0c
stagiar <stage@impactmediaconcepts.com>
2024-02-26 16:57:53 +0100

    category page backend

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
 {
     public function index(){
         return view('category.index',[
-            'categories' => Category::with(['child_categories', 'parent_category'])->get()
+            'categories' => Category::with(['child_categories'])->whereNull('parent_category_id')->get()
         ]);
     }
 
@@ -17,5 +17,24 @@ class CategoryController extends Controller
         return view('category.show',[
             'category' => $category
         ]);
+    }
+
+    public function create(){
+        return view('category.create',[
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function store(){
+        $request = request();
+
+        $attributes = $request->validate([
+            'name' => ['required'],
+            'parent_category_id' => ['nullable', 'numeric']
+        ]);
+
+        Category::create($attributes);
+
+        return redirect('/categories');
     }
 }
