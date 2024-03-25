e205ca78dd8e2aacc0c1653f161043549d9fbb8c
stagiar <stage@impactmediaconcepts.com>
2024-03-25 08:32:01 +0100

    softdeletes added

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Property extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     
     //Values begint met een type wat: multiselect, singleselect, number, text of bool kan zijn
     //als het type multi- of singelselect is dan is er ook een options velt waar de mogenlijkheden in staan
