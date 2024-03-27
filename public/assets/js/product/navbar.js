0133d2e2d26787957e9624a2fa5df32686e72d47
Stage-IMC <stage@impactmediaconcepts.com>
2024-03-27 15:22:51 +0100

    fixed product footer, adjusted sidenav code

--- public/assets/js/product/navbar.js
+++ public/assets/js/product/navbar.js
@@ -1,45 +1,35 @@
-let sideBar = document.querySelector(".side-bar");
-let activeItem = document.querySelectorAll(".side-bar-active-item");
-let arrowCollapse = document.querySelector(".open-button");
-let textToHide = document.querySelectorAll(".text");
+document.addEventListener("DOMContentLoaded", function() {
+    let sideNav = document.querySelector(".side-nav");
+    let activeItem = document.querySelectorAll(".side-nav-active-item");
+    let arrowCollapse = document.querySelector(".open-button");
+    let textToHide = document.querySelectorAll(".text");
+    
+    let isHidden = false;
 
-let isHidden = false;
-
-// Function to toggle sidenav state
-function toggleSidenav() {
-    arrowCollapse.classList.toggle("rotate-arrows");
-    sideBar.classList.toggle("close-sidenav");
-
-    const isCollapsed = sideBar.classList.contains("close-sidenav");
-    activeItem.forEach(element => {
-        element.style.width = isCollapsed ? "4.08rem" : "17.06rem";
-    });
-
-    const toggleTextVisibility = () => {
-        textToHide.forEach(element => {
-            element.classList.toggle("hidden", isCollapsed);
+    function toggleSidenav() {
+        arrowCollapse.classList.toggle("rotate-arrows");
+        sideNav.classList.toggle("close-sidenav");
+    
+        const isCollapsed = sideNav.classList.contains("close-sidenav");
+        activeItem.forEach(element => {
+            element.classList.toggle("close-sidenav", isCollapsed);
         });
-    };
-
-    if (!isHidden) {
-        toggleTextVisibility();
-        isHidden = true;
-    } else {
-        setTimeout(() => {
+    
+        const toggleTextVisibility = () => {
+            textToHide.forEach(element => {
+                element.classList.toggle("hidden", isCollapsed);
+            });
+        };
+    
+        if (!isHidden) {
             toggleTextVisibility();
-            isHidden = false;
-        }, 250);
+            isHidden = true;
+        } else {
+            setTimeout(() => {
+                toggleTextVisibility();
+                isHidden = false;
+            }, 250);
+        }
     }
-
-    // Store sidenav state in localStorage
-    localStorage.setItem('sidenavCollapsed', isCollapsed);
-}
-
-// Check if sidenav state is stored in localStorage
-const storedCollapsed = localStorage.getItem('sidenavCollapsed');
-if (storedCollapsed === 'true') {
-    toggleSidenav();
-}
-
-// Event listener for sidenav toggle
-arrowCollapse.onclick = toggleSidenav;
+    arrowCollapse.onclick = toggleSidenav;
+});
