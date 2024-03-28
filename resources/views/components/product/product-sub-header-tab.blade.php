f78f8b6415c135636acfe6c390ad8a75bf981c78
Stage-IMC <stage@impactmediaconcepts.com>
2024-03-28 08:36:02 +0100

    changed

--- resources/views/components/product/product-sub-header-tab.blade.php
+++ resources/views/components/product/product-sub-header-tab.blade.php
@@ -1,7 +1,7 @@
 @props(['text', 'orderId'])
 
 <button id="orderBy{{$orderId}}" class="flex justify-center items-center {{ $attributes->get('container-class', '') }} cursor-pointer">
-    <p class="text-[14px] mt-[0.12rem]">
+    <p class="{{ $text === 'Voorraad' ? 'w-6' : '' }} text-[14px] text-left mt-[0.12rem]">
         {{ $text }}
     </p>
     <img class="select-none ml-[0.37rem] mt-[0.38rem] w-[0.62rem] h-[0.37rem]" src="../images/arrow-down-icon.png">
