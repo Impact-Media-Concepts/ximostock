258e44280bf8ad6478f9c3131a8d82bce75bd70b
Stage-IMC <stage@impactmediaconcepts.com>
2024-03-28 12:01:37 +0100

    Update create.blade.php

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,4 +1,4 @@
-<!-- <x-layout._header-dependencies :sidenavActive="$sidenavActive" />
+<x-layout._header-dependencies :sidenavActive="$sidenavActive" />
 
 <body class="flex bg-[#F3F4F8] text-[#717171] text-[14px]" style="font-family: 'Inter', sans-serif;">
 <x-header.header/>
@@ -7,106 +7,107 @@
             <x-sidenav.sidenav :sidenavActive="$sidenavActive"/>
         </div>
         <div class="pt-20">
-            
+            <div>
+                <form method="POST" action="/products" enctype="multipart/form-data">
+                    @csrf
+                    <x-product.create.product-title />
+
+                    <label for="sku">Artikelnummer:</label>
+                    <input type="text" name="sku" id="sku" value="{{ old('sku') }}">
+
+                    <label for="ean">EAN: </label>
+                    <input type="number" name="ean" id="ean" value="{{ old('ean') }}">
+
+                    <label for="short_description"> korte beschrijving:</label>
+                    <input type="text" name="short_description" id="short_description" value="{{ old('short_description') }}">
+
+                    <label for="long_description"> lange beschrijving:</label>
+                    <input type="text" name="long_description" id="long_description" value="{{ old('long_description') }}">
+
+                    <label for="price">price: </label>
+                    <input type="number" step="0.01" name="price" id="price" value="{{ old('price') }}">
+
+                    <li>
+                        <x-product.create.categories.category-checkbox-list :categories="$categories"/>
+                    </li>
+
+                    <ul>
+                        <li>
+                            <label for="backorders">backorders</label>
+                            <input type="number" id="backorders" value="0" name="backorders">
+                        </li>
+                        <li>
+                            <label for="communicate_stock">communicate stock</label>
+                            <input type="number" checked id="communicate_stock" value="1" name="communicate_stock">
+                        </li>
+                    </ul>
+
+                    {{-- Photos --}}
+                    <div>
+                        <label for="primaryPhoto">primary Photo</label>
+                        <input type="file" id="primaryPhoto" name="primaryPhoto" />
+                    </div>
+
+                    <ul>
+                        <li><input type="file" id="photos1" name="photos[]" /></li>
+                        <li><input type="file" id="photos2" name="photos[]" /></li>
+                    </ul>
+
+                    <div>
+                        <h3>set eigenschappen</h3>
+                        @foreach ($properties as $property)
+                            @if ($property->type == 'text')
+                                <div>
+                                    <label for="property_{{ $property->id }}">{{ $property->name }}</label>
+                                    <input type="text" id="property_{{ $property->id }}" name="properties[{{ $property->id }}]">
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+
+                    <div>
+                        <h3>voorraden</h3>
+                        <label for="{{ $locations[0]->location_zones[0]->id }}">
+                            {{ $locations[0]->location_zones[0]->name }}
+                        </label>
+                        <input type="number" name="location_zones[{{ $locations[0]->location_zones[0]->id }}]">
+                        <label for="{{ $locations[0]->location_zones[1]->id }}">
+                            {{ $locations[0]->location_zones[1]->name }}
+                        </label>
+                        <input type="number" name="location_zones[{{ $locations[0]->location_zones[1]->id }}]">
+                    </div>
+
+                    <div>
+                        <h3>sales channels</h3>
+                        <ul>
+                            @foreach ($salesChannels as $channel)
+                                <li>
+                                    <label for="salesChannel[{{ $channel->id }}]">{{ $channel->name }}</label>
+                                    <input type="checkbox" name="salesChannels[]" value="{{ $channel->id }}"
+                                        id="salesChannel[{{ $channel->id }}]">
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    @if ($errors->any())
+                        <div>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <input type="submit" value="Submit"></input>
+                </form>
+            </div>
         </div>
     </div>
 </body>
 
-</html> -->
+<x-layout._footer-dependencies />
+<script type="text/javascript" src="{{ asset('./assets/js/product/navbar.js') }}"></script>
 
-<div class="">
-    <form method="POST" action="/products" enctype="multipart/form-data">
-        @csrf
-        <x-product.create.product-title />
-
-        <label for="sku">Artikelnummer:</label>
-        <input type="text" name="sku" id="sku" value="{{ old('sku') }}">
-
-        <label for="ean">EAN: </label>
-        <input type="number" name="ean" id="ean" value="{{ old('ean') }}">
-
-        <label for="short_description"> korte beschrijving:</label>
-        <input type="text" name="short_description" id="short_description" value="{{ old('short_description') }}">
-
-        <label for="long_description"> lange beschrijving:</label>
-        <input type="text" name="long_description" id="long_description" value="{{ old('long_description') }}">
-
-        <label for="price">price: </label>
-        <input type="number" step="0.01" name="price" id="price" value="{{ old('price') }}">
-
-        <li>
-            <x-product.create.categories.category-checkbox-list :categories="$categories"  :checkedCategories="$product->categories" />
-        </li>
-
-        <ul>
-            <li>
-                <label for="backorders">backorders</label>
-                <input type="number" id="backorders" value="0" name="backorders">
-            </li>
-            <li>
-                <label for="communicate_stock">communicate stock</label>
-                <input type="number" checked id="communicate_stock" value="1" name="communicate_stock">
-            </li>
-        </ul>
-
-        {{-- Photos --}}
-        <div>
-            <label for="primaryPhoto">primary Photo</label>
-            <input type="file" id="primaryPhoto" name="primaryPhoto" />
-        </div>
-
-        <ul>
-            <li><input type="file" id="photos1" name="photos[]" /></li>
-            <li><input type="file" id="photos2" name="photos[]" /></li>
-        </ul>
-
-        <div>
-            <h3>set eigenschappen</h3>
-            @foreach ($properties as $property)
-                @if ($property->type == 'text')
-                    <div>
-                        <label for="property_{{ $property->id }}">{{ $property->name }}</label>
-                        <input type="text" id="property_{{ $property->id }}" name="properties[{{ $property->id }}]">
-                    </div>
-                @endif
-            @endforeach
-        </div>
-
-        <div>
-            <h3>voorraden</h3>
-            <label for="{{ $locations[0]->location_zones[0]->id }}">
-                {{ $locations[0]->location_zones[0]->name }}
-            </label>
-            <input type="number" name="location_zones[{{ $locations[0]->location_zones[0]->id }}]">
-            <label for="{{ $locations[0]->location_zones[1]->id }}">
-                {{ $locations[0]->location_zones[1]->name }}
-            </label>
-            <input type="number" name="location_zones[{{ $locations[0]->location_zones[1]->id }}]">
-        </div>
-
-        <div>
-            <h3>sales channels</h3>
-            <ul>
-                @foreach ($salesChannels as $channel)
-                    <li>
-                        <label for="salesChannel[{{ $channel->id }}]">{{ $channel->name }}</label>
-                        <input type="checkbox" name="salesChannels[]" value="{{ $channel->id }}"
-                            id="salesChannel[{{ $channel->id }}]">
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        <input type="submit" value="Submit"></input>
-    </form>
-</div>
+</html>
