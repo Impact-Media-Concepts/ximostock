c7df19678e58b227dd5c3705084a917242e76dfd
stagiar <stage@impactmediaconcepts.com>
2024-03-28 08:22:56 +0100

    fixed default order by

--- resources/views/property/create.blade.php
+++ resources/views/property/create.blade.php
@@ -8,5 +8,33 @@
 </head>
 <body>
     <h1>TEST property create</h1>
+    <form action="/properties" method="POST">
+        @csrf
+        <label for="propName">naam:</label>
+        <input type="text" id="propName" name="name">
+        <label for="propType">type:</label>
+        <input type="text" id="propType" name="type">
+        <ul id="propOptions">
+            <li>
+                <label for="option1">option</label>
+                <input type="text" placeholder="option name..." name="options[]">
+                <button>-</button>
+            </li>
+            <li>
+                <label for="option1">option</label>
+                <input type="text" placeholder="option name..." name="options[]">
+                <button>-</button>
+            </li>
+            <li>
+                <label for="option2">option</label>
+                <input type="text" placeholder="option name..." name="options[]">
+                <button>-</button>
+                <button>+</button>
+            </li>
+        </ul>
+    </form>
+    <script>
+
+    </script>
 </body>
 </html>
