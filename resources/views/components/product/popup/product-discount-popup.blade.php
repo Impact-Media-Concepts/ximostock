dde1510761761abae631c2174fefbb2d2dedd514
Stage-IMC <stage@impactmediaconcepts.com>
2024-03-29 08:51:30 +0100

    started with discount popup

--- resources/views/components/product/popup/product-discount-popup.blade.php
+++ resources/views/components/product/popup/product-discount-popup.blade.php
@@ -1,3 +1,40 @@
+<style>
+
+    .discount-input::placeholder {
+    text-align: center; 
+    }
+
+    /* or, for legacy browsers */
+
+    .discount-input::-webkit-input-placeholder {
+    text-align: center;
+    }
+
+    .discount-input:-moz-placeholder { /* Firefox 18- */
+    text-align: center;  
+    }
+
+    .discount-input::-moz-placeholder {  /* Firefox 19+ */
+    text-align: center;  
+    }
+
+    .discount-input:-ms-input-placeholder {  
+    text-align: center; 
+    }
+    
+    /* Chrome, Safari, Edge, Opera */
+    .discount-input::-webkit-outer-spin-button,
+    .discount-input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+    }
+
+    /* Firefox */
+    .discount-input[type=number] {
+    -moz-appearance: textfield;
+    }
+</style>
+
 <div
     class="discount-popup w-full h-full fixed top-0 bg-black bg-opacity-75 hidden pt-32 select-none left-0" style="z-index: 999;"
 >
@@ -11,7 +48,7 @@
         no: 'Nee toch maar niet'
         }"
         x-transition
-        class="discount-popup-container relative w-[44rem] h-[24.81rem] bg-white pb-0 rounded-md text-center p-8 mx-auto mt-20 transform -translate-y-40 transition-transform duration-300"
+        class="discount-popup-container relative w-[44rem] h-[21.25rem] bg-white pb-0 rounded-md text-center p-8 mx-auto mt-20 transform -translate-y-40 transition-transform duration-300"
     >
         <div
             class="w-[2rem] discount-popup-close flex items-center relative bottom-4 left-[39rem] hover:cursor-pointer z-[1000]"
@@ -34,77 +71,44 @@
         </div>
        
         <div class="relative bottom-8">
-            <div class="w-[40.87rem] h-[19.31rem] mt-4">
+            <div class="w-[40.87rem] h-[15.81rem] mt-4" style="border: 1px solid #f0f0f0; border-radius 10px;">
                 <div class="w-[40.87rem] h-[2.5rem] bg-[#3DABD5] rounded-t-lg flex justify-start items-center pl-4 text-white"> 
                     <p>
                         Korting
                     </p>
                 </div>
-                <div class="h-[16.81rem] w-[40.87rem] rounded-b-lg" style="border: 1px solid #F0F0F0"> 
-                    <div class="w-[40.87rem] h-[10.31rem]">
-                        <div class="flex items-center gap-3.5">
-                            <div x-data="{ open: false, selectedProperty: '' }" class="relative flex items-center justify-start text-left bottom-[2.5rem] right-6">
-                                <input type="hidden" name="selected_property_id" x-bind:value="selectedProperty.id">
-                                <button type="button" @click="open = !open;"
-                                    class="flex items-center z-20 w-[38.06rem] px-[1.08rem] h-[2.68rem] text-sm font-light bottom-[0.05rem] border-1 border-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#717171] focus:ring-offset-2 focus:ring-offset-gray-100 relative left-12 top-[4rem]"
-                                    style="border: 1px solid #717171"  @click.away="open = false">
-                                    <!-- Display selected property name -->
-                                    <div class="flex mt-[0.08rem] relative right-[0.2rem]">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                            stroke="white" class="w-3 h-3">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                        </svg>
-                    
-                                    </div>
-                                    <span class="w-52 text-left text-[14px] text-gray-700 relative right-2" x-text="selectedProperty ? selectedProperty.name : 'Korting percentage'">
-                                    </span>
-                                </button>
-                        
-                                <div x-cloak x-show="open"
-                                    class="overflow-y-auto overflow-x-hidden absolute flex justify-center items-center h-[7.5rem] w-[38.06rem] bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-30 left-[3rem] top-[7.5rem]" style="border: 1px solid #F0F0F0;">
-                                    <ul >
-                                        <template  x-for="property in discount" :key="property.data_pages">
-                                            <li >
-                                                <button type="button" @click="selectedProperty = property; open = false;"
-                                                    class="hover:bg-[#3999BE] duration-100 block w-[38.06rem] px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none flex justify-start">
-                                                    <span  class="flex items-center justify-start pr-3 pl-4 " x-text="property.name"></span>
-                                                </button>
-                                            </li>
-                                        </template>
-                                    </ul>
-                                </div>
+                <div x-data="{ showDecimals: false }" class="pt-[1.5rem] pl-[1.5rem]">
+                    <div class="flex-col justify-center items-center">
+                        <div class="flex items-center">
+                            <div class="mr-[1rem]">
+                                <input class="text-center discount-input w-[18.68rem] h-[2.5rem] font-[16px] rounded-md" style="border: 1px solid #d3d3d3;" type="number" for="discountPercentage" placeholder="Kortingspercentage">
+                            </div>
+                            <div class="flex gap-[0.5rem]">
+                                <input type="checkbox" x-on:click="showDecimals = !showDecimals">
+                                <label class="text-[14px] font-bold" for="discountPercentage">Afronden op decimalen?</label>
                             </div>
                         </div>
                     </div>
-                    <div class="discount-buttons flex items-center gap-[0.7rem] absolute bottom-5 right-5">
+                    <div x-show="showDecimals" class="flex justify-start items-center pt-[0.5rem]">
+                        <input class="discount-input text-center w-[18.68rem] h-[2.5rem] font-[16px]rounded-md" style="border: 1px solid #d3d3d3;" type="number" for="discountDecimals" placeholder="Decimalen">
+                        <label for="discountDecimals"></label>
+                    </div>
+                </div>
+
+                <div class="discount-buttons flex items-center gap-[0.7rem] absolute right-[0.3rem]">
                         <button type="button" class="discount-popup-close flex justify-center gap-2 items-center discountCancel w-[7.87rem] h-[2.68rem] hover:bg-gray-100 rounded-md" style="border: 1px solid #717172;">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="gray"
-                                class="discount-popup-close flex items-center justify-center w-8 h-8 cursor-pointer"
-                            >
-                                <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M6 18 18 6M6 6l12 12"
-                                class="discount-popup-close "
-                                />
-                            </svg>
+                            <img class="select-none w-[0.8rem] h-[0.8rem] flex" src="../images/x-icon.png" alt="x icon">
                             <p class="discount-popup-close flex text-[#717171]">
                                 Annuleren
                             </p>
                         </button>
-                        <button type="button" class="flex justify-center items-center w-[7.87rem] h-[2.68rem] bg-[#3DABD5] rounded-md hover:bg-[#3999BE]">
+                        <button type="button" class="flex justify-center items-center w-[7.87rem] h-[2.68rem] bg-[#3DABD5] rounded-md hover:bg-[#3999BE] gap-[0.5rem]">
                             <img src="../images/save-icon.png">
                             <p class="flex text-[#F8F8F8]">
                                 Save
                             </p>
                         </button>
                     </div>
-                </div>
             </div>
         </div>
     </div>
