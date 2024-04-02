a3bbfc1da3e8f39f8ec4bb628c152a68a7b65010
stagiar <stage@impactmediaconcepts.com>
2024-04-02 13:58:38 +0200

    bulk discount

--- public/assets/js/product/manage-bulk-action-form.js
+++ public/assets/js/product/manage-bulk-action-form.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const BulkForm = document.getElementById('bulkActionsForm');
-
+    //delete
     const DeleteFormButton = document.getElementById('bulkActionArchive');
     DeleteFormButton.addEventListener('click', function(){
         BulkForm.action = '/products/bulkdelete';
     });
 
+    //discount
     const DiscountFormButton = document.getElementById('bulkActionDiscount');
     DiscountFormButton.addEventListener('click', function(){
         BulkForm.action = '/products/bulkdiscount';
     });
+    const round = document.getElementById('roundDiscount');
+    const trueRound = document.getElementById('trueRoundDiscount');
+    round.addEventListener('click', function(){
+        if(round.checked){
+            trueRound.value = 1;
+        }else{
+            trueRound.value = 0;
+        }
+    });
+
 });
