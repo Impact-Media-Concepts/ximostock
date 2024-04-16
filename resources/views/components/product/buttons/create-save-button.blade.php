a5266d313993f28f757739a78d74d7bcb29ee64d
Stage-IMC <stage@impactmediaconcepts.com>
2024-04-16 14:00:18 +0200

    started with categories styling, adjust stying for other steps

--- resources/views/components/product/buttons/create-save-button.blade.php
+++ resources/views/components/product/buttons/create-save-button.blade.php
@@ -1,15 +1,13 @@
 <button
-    class="hover:bg-[#3999BE] duration-100 flex items-center justify-center z-20 w-[10.53rem] px-[1.08rem] h-[2.78rem] text-sm font-light text-gray-700 bg-[#3dabd5] bottom-[0.05rem] border-1 border-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#3DABD5] focus:ring-offset-2 focus:ring-offset-gray-100 relative left-6 top-[0.02rem]"
+    class="hover:bg-[#3999BE] duration-100 flex items-center justify-center z-20 w-[10.53rem] px-[1.08rem] h-[2.78rem] text-sm font-light text-gray-700 bg-[#3dabd5] bottom-[0.05rem] border-1 border-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#3DABD5] focus:ring-offset-2 focus:ring-offset-gray-100 relative right-[1.4rem] top-[0.02rem]"
     style="border: 1px solid white"
     id="saveBtn"
     type="submit">
     <div class="flex items-center justify-center">
         <div class="flex mt-[0.08rem]">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-            </svg>
+            <img class="select-none flex" src="../images/edit-icon.png" alt="edit icon">
         </div>
-        <span class="pl-[0.2rem] text-left text-[14px] text-white">Aanmaken</span>
+        <span class="pl-[0.5rem] text-left text-[14px] text-white">Aanmaken</span>
     </div>
 </button>
   
