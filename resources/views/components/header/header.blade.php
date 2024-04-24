264cae7d3a81f81005266c39e7865a44b746cb7e
Stage-IMC <stage@impactmediaconcepts.com>
2024-04-24 08:32:03 +0200

    small header styling update

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -222,13 +222,8 @@
                 <button @click="open = !open;"
                     class="flex items-center z-20 w-[9rem] px-[1.08rem] h-[2.68rem] text-sm font-light bottom-[0.05rem] border-1 border-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#717171] focus:ring-offset-2 focus:ring-offset-gray-100 relative left-6 top-[0.02rem]"
                     style="border: 1px solid #717171" type="button" @click.away="open = false">
-                    <div class="flex mt-[0.08rem] relative right-[0.2rem]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="white" class="w-3 h-3">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                        </svg>
-                    </div>
-                    <span class="w-52 text-left text-[14px] text-gray-700 line-clamp-1 relative right-2"
+                    
+                    <span class="text-[14px] text-gray-700 line-clamp-1 relative right-2"
                         title="{{ Auth::user()->name }}">
                         {{ Auth::user()->name }}
                     </span>
