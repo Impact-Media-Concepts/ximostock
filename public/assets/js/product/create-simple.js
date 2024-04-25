34c9b5b1536a8352d8969ccd32a060e5205b964d
Stage-IMC <stage@impactmediaconcepts.com>
2024-04-25 14:43:05 +0200

    changes

--- public/assets/js/product/create-simple.js
+++ public/assets/js/product/create-simple.js
@@ -117,30 +117,53 @@ async function handleFileInputChange(event) {
     const files = event.target.files;
     
     if (files.length === 1) {
-        await uploadSingleFile(files[0]);
+        await uploadSingleFile(files[0], event);
     } else if (files.length > 1) {
-        await uploadMultipleFiles(files);
+        await uploadMultipleFiles(files, event);
     }
 }
 
 //upload a single file
-async function uploadSingleFile(file) {
+async function uploadSingleFile(file, event) {
     splideSection.classList.remove('hidden');
-    const img = createImageElement(file);
-    const li = createSlideElement(img, file.name);
+    const fotoInputTag = document.createElement('input');
+
+    fotoInputTag.name = 'photos[]';
+    fotoInputTag.type = 'file';
+    fotoInputTag.classList.add('hidden');
+
+    if (event.target.files[0]) {
+        fotoInputTag.files = event.target.files;
+    }
+
+    primaryPhotoInput = fotoInputTag;
+
+    const img = createImageElement(file, fotoInputTag);
+    const li = createSlideElement(img, file.name, fotoInputTag);
+
     splideList.appendChild(li);
     splide.refresh();
 }
 
 //upload multiple files
-async function uploadMultipleFiles(files) {
+async function uploadMultipleFiles(files, event) {
     splideSection.classList.remove('hidden');
     // Create a document fragment to hold the elements
     const fragment = document.createDocumentFragment();
 
     for (const file of files) {
-        const img = createImageElement(file);
-        const li = createSlideElement(img, file.name);
+        const fotoInputTag = document.createElement('input');
+        fotoInputTag.name = 'photos[]';
+        fotoInputTag.type = 'file';
+        fotoInputTag.classList.add('hidden');
+
+        if (event.target.files[0]) {
+            fotoInputTag.files = event.target.files;
+        }
+
+        const img = createImageElement(file, fotoInputTag);
+        const li = createSlideElement(img, file.name, fotoInputTag);
+    
         fragment.appendChild(li);
     }
 
@@ -150,20 +173,24 @@ async function uploadMultipleFiles(files) {
 }
 
 //create image element
-function createImageElement(file) {
+function createImageElement(file, fotoInputTag) {
     const img = document.createElement('img');
     img.src = URL.createObjectURL(file);
     img.classList.add('object-contain', 'select-image', '!w-[16rem]', 'select-none');
-    img.addEventListener('click', handleImageClick);
+    
+    img.onclick = function (event) {
+        handleImageClick(fotoInputTag, event);
+    };
+
     return img;
 }
 
 //create splide slide element
-function createSlideElement(img, fileName) {
+function createSlideElement(img, fileName, fotoInputTag) {
     const li = document.createElement('li');
     li.classList.add('splide__slide', 'pt-[1.5rem]');
     
-    const imgContainer = createImageContainer(img);
+    const imgContainer = createImageContainer(img, fotoInputTag);
     li.appendChild(imgContainer);
     
     const span = document.createElement('span');
@@ -177,7 +204,7 @@ function createSlideElement(img, fileName) {
 }
 
 //create image container
-function createImageContainer(img) {
+function createImageContainer(img, fotoInputTag) {
     const imgContainer = document.createElement('span');
     imgContainer.classList.add('relative');
     imgContainer.style.width = '254px';
@@ -189,6 +216,7 @@ function createImageContainer(img) {
     const removeButton = createRemoveButton();
     imgContainer.appendChild(removeButton);
     imgContainer.appendChild(img);
+    imgContainer.appendChild(fotoInputTag);
     
     return imgContainer;
 }
@@ -211,11 +239,13 @@ function createRemoveButton() {
 }
 
 //image click
-function handleImageClick() {
+function handleImageClick(fotoInputTag, event) {
     const images = document.querySelectorAll('.select-image');
     images.forEach(image => image.classList.remove('border-[3px]', 'border-[#3EABD5]'));
-    this.classList.add('border-[3px]', 'border-[#3EABD5]');
-    setPrimary.onclick = handleSetPrimaryClick(this);
+    const f = event.target;
+    f.classList.add('border-[3px]', 'border-[#3EABD5]');
+    setPrimary.onclick = handleSetPrimaryClick(f, fotoInputTag);
+
 }
 
 //remove button click
@@ -229,49 +259,46 @@ function handleRemoveButtonClick() {
 }
 
 //primary click
-function handleSetPrimaryClick(img) {
+function handleSetPrimaryClick(img, fotoInputTag) {
     return function() {
-        console.log("clean img", img);
         if (img.classList.contains("primary-photo-size")) {
-            console.log("unset img", img);
             unsetPrimaryPhoto(img);
         } else {
             img.classList.add("primary-photo-size");
             const imgContainer = img.parentElement;
+            const liContainer = imgContainer.parentElement;
+            console.log("Li container: ", liContainer);
             imgContainer.classList.add("primary-span-size");
             const lis = document.querySelectorAll('.splide__slide');
             lis.forEach(li => li.classList.remove('primary-size'));
             const li = imgContainer.parentElement;
             li.classList.add('primary-size');
-            
-            const primaryPhotoInput = document.querySelector('input[name="primaryPhoto"]');
-            if (primaryPhotoInput) {
+            console.log(" imgContainer", imgContainer);
+            console.log(" li", li);
+            fotoInputTag.name = 'primaryPhoto';
+            console.log("primaryPhotoInput:", primaryPhotoInput);
+            if (primaryPhotoInput && primaryPhotoInput !== fotoInputTag) {
                 primaryPhotoInput.name = 'photos[]';
             }
-            const photoInputTag = imgContainer.querySelector('input[type="file"]');
-            if (photoInputTag) {
-                photoInputTag.name = 'primaryPhoto';
-            }
+            
+            primaryPhotoInput = fotoInputTag;
         }
     };
 }
 
-function unsetPrimaryPhoto(img) {
-    console.log("unsetprimaryPhoto function", img);
-    img.classList.remove("primary-photo-size");
-    const imgContainer = img.parentElement;
-    imgContainer.classList.remove("primary-span-size");
-    const lis = document.querySelectorAll('.splide__slide');
-    lis.forEach(li => li.classList.remove('primary-size'));
-    const primaryPhotoInput = document.querySelector('input[name="photos[]"]');
-    if (primaryPhotoInput) {
-        primaryPhotoInput.name = 'primaryPhoto';
-    }
-    const photoInputTag = imgContainer.querySelector('input[type="file"]');
-    if (photoInputTag) {
-        photoInputTag.name = 'photos[]';
-    }
-}
+// function unsetPrimaryPhoto(img) {
+//     console.log("unsetprimaryPhoto function", img);
+//     img.classList.remove("primary-photo-size");
+//     const imgContainer = img.parentElement;
+//     imgContainer.classList.remove("primary-span-size");
+//     const lis = document.querySelectorAll('.splide__slide');
+//     lis.forEach(li => li.classList.remove('primary-size'));
+//     const primaryPhotoInput = document.querySelector('input[name="photos[]"]');
+//     if (primaryPhotoInput && primaryPhotoInput !== fotoInputTag) {
+//         primaryPhotoInput.name = 'photos[]';
+//         console.log("primaryPhotoInput if not primary:", primaryPhotoInput);
+//     }
+// }
 
 document.getElementById('get_file').onclick = function() {
     fileInput.click();
