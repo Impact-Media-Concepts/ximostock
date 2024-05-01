dfe0000eb7b973fc5779496c296946c5ec5e92b5
Stage-IMC <stage@impactmediaconcepts.com>
2024-05-01 16:27:12 +0200

    added layout for pages

---
+++ resources/views/components/layout/_layout.blade.php
@@ -0,0 +1,18 @@
+<!-- <x-layout._header-dependencies :sidenavActive="$sidenavActive" />
+
+<body class="flex bg-[#F3F4F8] text-[#717171] text-[14px]" style="font-family: 'Inter', sans-serif;">
+<x-header.header :activeWorkspace="$activeWorkspace" :workspaces="$workspaces"/>
+    <div class="flex h-full pt-20 w-full gap-[1.9rem]">
+        <div class="h-full">
+            <x-sidenav.sidenav :activeWorkspace="$activeWorkspace" :sidenavActive="$sidenavActive"/>
+        </div>
+
+        </div>
+</body>
+
+<x-layout._footer-dependencies />
+<script type="text/javascript" src="{{ asset('./assets/js/product/navbar.js') }}"></script>
+<script type="text/javascript" src="{{ asset('./assets/js/product/create-simple.js') }}"></script>
+<script type="text/javascript" src="{{ asset('./assets/js/product/show-pop-ups.js') }}"></script>
+
+</html> -->
