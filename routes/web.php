9161cd3ef4083b43ff3d97c09eaf15a214b7143b
stagiar <stage@impactmediaconcepts.com>
2024-05-23 12:27:29 +0200

    some minor fixes

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/products', [ProductController::class, 'index'])->middleware('can:viewAny,App\Models\Product');
     Route::get('/products/create', [ProductController::class, 'create'])->middleware('can:create,App\Models\Product');
     Route::get('/products/export/', [ProductController::class, 'export']);//export
-    Route::get('/products/archive',[ProductController::class, 'archive']);//can delete // can restore
+    Route::get('/products/archive',[ProductController::class, 'archive'])->middleware('can:forceDelete,App\Models\Product')->middleware('can:restore,App\Models\Product');
     Route::get('/products/{product}', [ProductController::class, 'show'])->middleware('can:view,product');
     Route::post('/products', [ProductController::class, 'store']);//autherized in controller
     Route::patch('/products/{product}', [ProductController::class, 'update']);//autherized in controller
@@ -45,52 +45,51 @@ Route::middleware('auth')->group(function () {
     Route::post('/products/bulkdisablebackorders', [ProductController::class, 'bulkDisableBackorders'])->name('products.bulkDisableBackorders');//autherized in controller
     Route::post('/products/bulkenablecommunicateStock', [ProductController::class, 'bulkEnableCommunicateStock'])->name('products.bulkEnableCommunicateStock');//autherized in controller
     Route::post('/products/bulkdisablecommunicateStock', [ProductController::class, 'bulkDisableCommunicateStock'])->name('products.bulkDisableCommunicateStock');//autherized in controller
-    Route::post('/products/restore', [ProductController::class, 'restore']); //can restore
-    Route::post('/products/forcedelete', [ProductController::class, 'forceDelete']); //can delete
+    Route::post('/products/restore', [ProductController::class, 'restore'])->middleware('can:restore,App\Models\Product');
+    Route::post('/products/forcedelete', [ProductController::class, 'forceDelete'])->middleware('can:forceDelete,App\Models\Product');
 
     //variant product
-    Route::get('/products/variant/create', [ProductVariationController::class, 'create']);
-    Route::post('/products/variant', [ProductVariationController::class, 'store']);
+    Route::get('/products/variant/create', [ProductVariationController::class, 'create'])->middleware('can:create,App\Models\Product');
+    Route::post('/products/variant', [ProductVariationController::class, 'store']);//autherized in controller
 
     //categories
     Route::get('/categories', [CategoryController::class, 'index'])->middleware('can:viewAny,App\Models\Category');
     Route::get('/categories/create', [CategoryController::class, 'create'])->middleware('can:create,App\Models\Category');
-    Route::get('/categories/archive', [CategoryController::class, 'archive']);
+    Route::get('/categories/archive', [CategoryController::class, 'archive'])->middleware('can:restore,App\Models\categories')->middleware('can:forceDelete,App\Models\categories');
     Route::get('/categories/{category}', [CategoryController::class, 'show'])->middleware('can:view,category');
     Route::post('/categories', [CategoryController::class, 'store']);//autherized in controller
     Route::patch('/categories/{category}', [CategoryController::class, 'update']);//autherized in controller
     Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->middleware('can:delete,category');
-    Route::post('/categories/restore',[CategoryController::class, 'restore']);//can restore
-    Route::post('/categories/forcedelete',[CategoryController::class, 'forceDelete']);//can delete
+    Route::post('/categories/restore',[CategoryController::class, 'restore'])->middleware('can:restore,App\Models\categories');
+    Route::post('/categories/forcedelete',[CategoryController::class, 'forceDelete'])->middleware('can:forceDelete,App\Models\categories');
 
     //properties
     Route::get('/properties', [PropertyController::class, 'index'])->middleware('can:viewAny,App\Models\Property');
     Route::get('/properties/create', [PropertyController::class, 'create'])->middleware('can:create,App\Models\Property');
-    Route::get('/properties/archive',[PropertyController::class, 'archive']);//can delete // can restore
+    Route::get('/properties/archive',[PropertyController::class, 'archive'])->middleware('can:restore,App\Models\Property')->middleware('can:foreceDelete,App\Models\Property');
     Route::get('/properties/{property}', [PropertyController::class, 'show'])->middleware('can:view,property');
     Route::post('/properties',[PropertyController::class, 'store'])->middleware('can:create,App\Models\Property');
     Route::patch('/properties/{property}', [PropertyController::class, 'update'])->middleware('can:update,property');
     Route::post('/properties/bulkdelete', [PropertyController::class, 'bulkDelete']);//autherized in controller
-    Route::post('/properties/restore',[PropertyController::class, 'restore']);//can restore
-    Route::post('/properties/forcedelete',[PropertyController::class, 'forceDelete']);//can delete
+    Route::post('/properties/restore',[PropertyController::class, 'restore'])->middleware('can:restore,App\Models\Property');
+    Route::post('/properties/forcedelete',[PropertyController::class, 'forceDelete'])->middleware('can:foreceDelete,App\Models\Property');
 
     //salesChannels
     Route::get('/saleschannels', [SalesChannelController::class, 'index'])->middleware('can:viewAny,App\Models\SalesChannel');
     Route::get('/saleschannels/create',[SalesChannelController::class, 'create'])->middleware('can:create,App\Models\SalesChannel');
-    Route::get('/saleschannels/archive',[SalesChannelController::class, 'archive']);//can delete // can restore
+    Route::get('/saleschannels/archive',[SalesChannelController::class, 'archive'])->middleware('can:restore,App\Models\SalesChannel')->middleware('can:foreceDelete,App\Models\SalesChannel');
     Route::get('/saleschannels/{salesChannel}', [SalesChannelController::class, 'show'])->middleware('can:view,salesChannel');
     Route::post('/saleschannels', [SalesChannelController::class, 'store'])->middleware('can:create,App\Models\SalesChannel');
     Route::patch('/saleschannels/{salesChannel}', [SalesChannelController::class, 'update'])->middleware('can:update,salesChannel');
     Route::post('/saleschannels/bulkdelete', [SalesChannelController::class, 'bulkDelete']);//autherized in controller
-    Route::post('/saleschannels/restore',[SalesChannelController::class, 'restore']);//can restore
-    Route::post('/saleschannels/forcedelete',[SalesChannelController::class, 'forceDelete']);//can delete
+    Route::post('/saleschannels/restore',[SalesChannelController::class, 'restore'])->middleware('can:restore,App\Models\SalesChannel');
+    Route::post('/saleschannels/forcedelete',[SalesChannelController::class, 'forceDelete'])->middleware('can:foreceDelete,App\Models\SalesChannel');
 
     //InentoryLocations
-    Route::get('/locations', [InventoryLocationController::class, 'index']); //can view any
-    Route::get('/locations/create', [InventoryLocationController::class, 'create']); //can create
-    Route::get('/locations/{location}', [InventoryLocationController::class, 'show']); //can view
-    Route::post('/locations', [InventoryLocationController::class, 'store']); //create
-
+    Route::get('/locations', [InventoryLocationController::class, 'index'])->middleware('can:viewAny,App\Models\InventoryLocation');
+    Route::get('/locations/create', [InventoryLocationController::class, 'create'])->middleware('can:create,App\Models\InventoryLocation');
+    Route::get('/locations/{location}', [InventoryLocationController::class, 'show'])->middleware('can:view,location');
+    Route::post('/locations', [InventoryLocationController::class, 'store'])->middleware('can:create,App\Models\InventoryLocation');
 
     //Users (only admins can access these)
     Route::get('/users', [UserController::class, 'index'])->middleware('can:viewAny,App\Models\User');
