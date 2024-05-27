b747993ee3659afacf6e00ad6038ff8bd60119ce
Stage-IMC <stage@impactmediaconcepts.com>
2024-05-27 08:29:01 +0200

    test ssh git

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,7 +30,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Maatwebsite\Excel\Facades\Excel;
-use Spatie\Activitylog\Facades\LogBatch;
+
 
 class ProductController extends BaseProductController
 {
@@ -179,12 +179,6 @@ class ProductController extends BaseProductController
 
         Gate::authorize('destroy-product', $product);
 
-
-        activity()
-            ->causedBy(Auth::user())
-            ->performedOn($product)
-            ->log('deleted');
-
         // Soft delete the product
         $product->delete();
 
@@ -201,11 +195,10 @@ class ProductController extends BaseProductController
         ]);
 
         // Delete selected products
-        LogBatch::startBatch();
         $woocommerce = new WooCommerceManager;
         $woocommerce->deleteProductsFromAll($validatedData['product_ids']);
         Product::whereIn('id', $validatedData['product_ids'])->delete();
-        LogBatch::endBatch();
+
         return redirect()->back();
     }
 
@@ -220,7 +213,7 @@ class ProductController extends BaseProductController
             'cents' => ['nullable', 'numeric', 'digits_between:0,2', 'Integer', 'min:0'],
             'round' => ['required', 'boolean']
         ]);
-        LogBatch::startBatch();
+
         // Apply the discount to each product
         if ($validatedData['discount'] != 0) {
             if ($validatedData['round']) {
@@ -261,7 +254,6 @@ class ProductController extends BaseProductController
                 $product->save();
             }
         }
-        LogBatch::endBatch();
         return redirect()->back();
     }
 
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,20 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Property extends Model
 {
-    use HasFactory, SoftDeletes, LogsActivity;
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()
-        ->logAll()
-        ->logOnlyDirty();
-    }
-
+    use HasFactory, SoftDeletes;
+    
     //Values begint met een type wat: multiselect, singleselect, number, text of bool kan zijn
     //als het type multi- of singelselect is dan is er ook een options velt waar de mogenlijkheden in staan
 
