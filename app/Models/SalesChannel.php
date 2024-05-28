c0dca9aa8638488a3d982d415591a4fa49c26078
Stage-IMC <stage@impactmediaconcepts.com>
2024-05-28 09:19:32 +0200

    axpanded logging and wocommerce remove categorie fucntions

--- app/Models/SalesChannel.php
+++ app/Models/SalesChannel.php
@@ -5,14 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class SalesChannel extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
 
     protected $guarded = ['id'];
 
-
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logAll()
+        ->logOnlyDirty();
+    }
+    
     public function products()
     {
         return $this->belongsToMany(Product::class, 'product_sales_channel')
