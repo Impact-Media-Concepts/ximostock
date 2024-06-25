020366b8d2e1418dd60af79c260d46c2e867919d
Jordi-IMC <94919375+Jordi-IMC@users.noreply.github.com>
2024-06-25 14:23:56 +0200

    Added vue functionality and scss files

--- resources/views/components/layout/_footer-dependencies.blade.php
+++
@@ -1,3 +0,0 @@
-<!-- adds jquery & alpinejs -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
